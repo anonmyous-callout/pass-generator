ca6e37db1b9ba402735d3b2e113348ed207b1971
Joker <96026123+anonmyous-callout@users.noreply.github.com>
2021-12-15 03:22:00 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -1,24 +1,38 @@
+#Random Password Generator
+
+
+#Importing Modules
+
+
 import random
+import re
+import subprocess
+import sys
+import time
 from os import system, name
 from time import sleep
-import time, sys 
-import re 
-import subprocess
+
+
+# Clearing The Terminal Screen
+
 
 def clear():
-
     if name == 'nt':
-        _ = system('cls')
+        _ = system('cls')   #For Windows
     else:
-        _ = system('clear')
+        _ = system('clear') #For MacOS
+
+
 clear()
 
+
+#Check For Special Required Module
 
 
 def check():
     print("\u001b[32;1m[+] CHECKING REQUIRED MODULES !\u001b[32;1m")
     try:
-        import tqdm 
+        import tqdm
         print("\n\u001b[32;1m[+] All Required Modules Found !\u001b[32;1m")
         time.sleep(2)
     except ModuleNotFoundError:
@@ -28,50 +42,61 @@ def check():
         time.sleep(2)
         subprocess.call("pip install tqdm", shell=True)
         time.sleep(1.5)
+
+
 check()
-
-
-
-
-
 
 
 from tqdm import tqdm
 
 
-#!/usr/bin/python
+# !/usr/bin/python
 # vim: set fileencoding= ANSI
 
 
-
-
+# Clearing The Terminal Screen
 
 
 def clear():
-
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system('clear')
+
+
 clear()
 
+
+# Loading Bar Animation
+
+
 def loading():
-    for i in tqdm(range(100), desc = "Loading", unit = " keystrokes "):
+    for _ in tqdm(range(100), desc="Loading", unit=" keystrokes "):
         time.sleep(0.1)
-        # vim: set fileencoding= ANSI
+        
+
+
 print("\u001b[31;1mLoading PassGen ♪♪ ....")
-print("Be Pateints !\u001b[31;1m")
-print(" \u001b[31;1m ") 
+print("Be Patients !\u001b[31;1m")
+print(" \u001b[31;1m ")
 print("\u001b[31;1m")
 loading()
 
-def clear():
 
+# Clearing The Terminal Screen
+
+
+def clear():
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system('clear')
+
+
 clear()
+
+
+# Banner Of The Generator
 
 
 def banner():
@@ -83,11 +108,19 @@ def banner():
     print(u"\u001b[31m                                  /____/      \u001b[36m             \u001b[31m    \u001b[0m   ")
     print("\t \u001b[31m by:- \u001b[36m ♦♣ JOKERS™ \u001b[36m♣♦ \u001b[31m \u001b[0m ")
 
+
 banner()
+
+
+# Main Code 
+# Making Choices
+
 
 print("\n\n\u001b[36;1m[1] Default Length Password Generation")
 print("[2] Customized Length Password Generation")
 print("[3] Exit\u001b[36;1m")
+
+
 while True:
     try:
         choice = int(input("\n\u001b[33;1m[→] Enter Your Choice : \u001b[33;1m"))
@@ -95,6 +128,7 @@ while True:
     except ValueError:
         print("\u001b[31;1m[!] Invalid Option.\n[!] Try Again !\u001b[31;1m ")
         continue
+
 
 while choice < 1:
     print("\u001b[31;1m[!] Invalid Option !\n[!] Try Again !\u001b[31;1m ")
@@ -104,11 +138,13 @@ while choice < 1:
             break
         except ValueError:
             print("\u001b[31;1m[!] Invalid Option.\n[!] Try Again ! \u001b[31;1m")
-while choice < 1 :
+
+
+while choice < 1:
     print("\u001b[31;1m[!] Invalid Option ! \n[!] Try Again !\u001b[31;1m ")
     break
     continue
-while choice > 3 :
+while choice > 3:
     print("\u001b[31;1m[!] Invalid Option !\n[!] Try Again !\u001b[31;1m ")
     while True:
         try:
@@ -116,50 +152,56 @@ while choice > 3 :
             break
         except ValueError:
             print("\u001b[31;1m[!] Invalid Option.\n[!] Try Again ! \u001b[31;1m")
-while choice > 3 :
+
+
+while choice > 3:
     print("\u001b[31;1m[!] Invalid Option ! \n[!] Try Again !\u001b[31;1m ")
     break
     continue
+
+
 while choice == 1:
     len = 18
     chacrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
-    key = ''.join(random.choice(chacrs)for x in range(len))
+    key = ''.join(random.choice(chacrs) for x in range(len))
     print("\n\u001b[33;1m[+] Password Is Being Generated..! Be Pateints\u001b[33;1m")
-    print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[",key, "]")
-    client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m") 
+    print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[", key, "]")
+    client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m")
     case = client.upper()
     while not re.match("^[Y&N]*$", case):
-        print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+        print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
         client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [Default N] : \u001b[32;1m")
         case = client.upper()
-        while not re.match("^[Y&N&" "]*$", case):
-            print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
-            break 
+        while not re.match("^[Y&N]*$", case):
+            print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+            break
             continue
-            if (case == "N" or " "):
+            if case == "N" or " ":
                 print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \u001b[34m  \u001b[32m \t")
-    while (case=="Y" ):
+    while case == "Y":
         len = 18
         chacrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
-        key = ''.join(random.choice(chacrs)for x in range(len))
+        key = ''.join(random.choice(chacrs) for x in range(len))
         print("\n\u001b[33;1m[+] Password Is Being Generated..! Be Pateints\u001b[33;1m")
-        print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[",key, "]")
-        client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m") 
+        print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[", key, "]")
+        client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m")
         case = client.upper()
         while not re.match("^[Y&N]*$", case):
-            print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+            print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
             client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n : \u001b[32;1m")
             case = client.upper()
-        while not re.match("^[Y&N&" "]*$", case):
-            print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
-            break 
+        while not re.match("^[Y&N]*$", case):
+            print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+            break
             continue
-            while (case =="Y"):
+            while case == "Y":
                 continue
-    while (case=="N" or " "):
+    while case == "N" or " ":
         print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \u001b[34m  \u001b[32m \t")
         break
     break
+
+
 
 if choice == 2:
     def password():
@@ -172,7 +214,7 @@ if choice == 2:
                 print("\u001b[31;1m[!] Enter Length In Numbers.\u001b[31;1m")
                 continue
         chacrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
-        while (len<8):
+        while len < 8:
             print("\u001b[31;1m[!] Cannot Generate Password Less Than 8. It Can Be Hacked Easily !\u001b[31;1m")
             print("\u001b[31;1m[!] Aborting !\u001b[31;1m")
             print("\u001b[32m[+] Continuing..! \u001b[32m ")
@@ -186,23 +228,23 @@ if choice == 2:
             chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
             continue
             break
-        if (len>=8):
+        if len >= 8:
             print("\u001b[33;1m[+] Password Is Being Generated..! Be Pateints\u001b[33;1m")
-            authenticationkey = ''.join(random.choice(chacrs)for _ in range (len)) 
-            print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[",authenticationkey, "]")
-            client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m") 
+            authenticationkey = ''.join(random.choice(chacrs) for _ in range(len))
+            print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[", authenticationkey, "]")
+            client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [ Default N]: \u001b[32;1m")
             case = client.upper()
-            while not re.match("^[Y&N&" "]*$", case):
-                print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+            while not re.match("^[Y&N]*$", case):
+                print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
                 client = input("\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [Default N]: \u001b[32;1m")
                 case = client.upper()
-            while not re.match("^[Y&N&" "]*$", case):
-                print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
-                break 
+            while not re.match("^[Y&N]*$", case):
+                print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+                break
                 continue
-                if (case == "N" or " "):
+                if case == "N" or " ":
                     print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \t \n \u001b[34m")
-            while (case=="Y"):
+            while case == "Y":
                 while True:
                     try:
                         len = int(input("\n\u001b[36;1m[→] Enter The Length Of The Password : \u001b[36;1m"))
@@ -210,39 +252,42 @@ if choice == 2:
                     except ValueError:
                         print("\u001b[31;1m[!] Enter Length In Numbers.\u001b[31;1m")
                 chacrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
-                while (len<8):
+                while len < 8:
                     print("\u001b[31;1m[!] Cannot Generate Password Less Than 8. It Can Be Hacked Easily !\u001b[31;1m")
                     print("\u001b[31;1m[!] Aborting !\u001b[31;1m")
                     print("\u001b[32m[+] Continuing\u001b[32m")
                     len = int(input("\n\u001b[34;1m[→] Enter The Length Of The Password : \u001b[34;1m"))
-                    chacrs ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
+                    chacrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%_-^&*():<?';/.,'[]{}\|/*-++?"
                     continue
                     break
-                while (len>=8):
+                while len >= 8:
                     print("\u001b[33;1m[+] Password Is Being Generated..! Be Pateints\u001b[33;1m")
-                    authenticationkey = ''.join(random.choice(chacrs)for _ in range (len)) 
-                    print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[",authenticationkey, "]")
+                    authenticationkey = ''.join(random.choice(chacrs) for _ in range(len))
+                    print("\u001b[32;1m[+] Your Authentication Key Is : \u001b[37;1m""[", authenticationkey, "]")
                     break
                 client = input("\n\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [Default N]: \u001b[32;1m")
                 case = client.upper()
-                while not re.match("^[Y&N&" "]*$", case):
-                    print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+                while not re.match("^[Y&N]*$", case):
+                    print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
                     client = input("\u001b[32;1m[→] Do You Want To Change The PassPhrase y/n [Default N]: \u001b[32;1m")
                     case = client.upper()
-                while not re.match("^[Y&N&" "]*$", case):
-                    print ("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
-                    break 
+                while not re.match("^[Y&N]*$", case):
+                    print("\u001b[31;1m[!] Error! Only letters Y/N allowed!\u001b[31;1m")
+                    break
                     continue
-                    while (case=="Y" ):
+                    while case == "Y":
                         continue
-                    while (case=="N" or " "):
+                    while case == "N" or " ":
                         print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \u001b[34m  \u001b[32m \t")
                         break
-            while (case=="N" or " "):
+            while case == "N" or " ":
                 print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \u001b[34m  \u001b[32m \t")
                 break
 
+
     password()
+
+
 while choice == 3:
     print("\n\u001b[31;1m[+] Quiting !\u001b[31;1m")
     print("\t \n \u001b[34m -----------Thank You For Using PassGen ♪♪----------- \u001b[34m  \u001b[32m \t")
